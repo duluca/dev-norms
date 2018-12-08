2257980155aaeb9222bdf8d528b37f2887cc3e94
Doguhan Uluca <duluca@gmail.com>
2018-12-08 03:15:35 -0500

    updated libraries, simplified code, added better instructions

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var fs = require('fs');
-var mknorms = require('./lib/create-norms');
-
-if (fs.existsSync('dev-norms.md')) {
-    var baseDir = process.argv[2] || '';
-    mknorms(baseDir);
-}
+require('./lib/dev-norms')
